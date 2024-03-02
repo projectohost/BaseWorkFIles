4ca98a61d14f719827afd446bd803ebe5d7f6db1
QueOta <queota@supermail.com>
2024-03-02 11:47:28 +0200

    firstTask -- Completed

--- FirstTask/quotes_prog.py
+++ FirstTask/quotes_prog.py
@@ -1,1 +1,24 @@
 #напиши тут свою програму
+with open('./FirstTask/quotes.txt', "r", encoding = "UTF-8") as file:
+    for line in file:
+        print(line)
+
+author = input("Хто написав ці рядки? ")
+with open('./FirstTask/quotes.txt', "a",  encoding = "UTF-8") as file:
+    file.write(f"({author})\n")
+
+while True:
+    answer = input("Бажаєте додати ще одну цитату? (так / ні)")
+    answer = answer.lower()
+    if answer == "так":
+        quote = input("Введіть цитату: ")
+        author = input("Введіть автора: ")
+        file = open("./FirstTask/quotes.txt", "a",  encoding = "UTF-8")
+        file.write(f"{quote}\n({author})\n")
+        file.close()
+    else:
+        break
+
+with open('./FirstTask/quotes.txt', "r", encoding = "UTF-8") as file:
+    for line in file:
+        print(line)
